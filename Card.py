0b22e49cc7f855323412112a3f01a2ecce53ab08
Shajar Urstain <shajar.urstain@gmail.com>
2024-10-07 11:14:45 +0300

    fixed Card

--- Card.py
+++ Card.py
@@ -1,5 +1,6 @@
 from os import PathLike
-
+from sympy.codegen.cnodes import struct
+import struct
 from CryptImage import CryptImage
 from PIL import Image
 from Crypto.Cipher import AES
@@ -7,7 +8,9 @@ import hashlib
 from typing import Union
 
 class Card:
-    bytesize = 4
+    hashlen = 32
+    fmt = 'I'*5
+    leng = 20
 
     def __init__(self, name: str, author: str, riddle: str, sol: Union[str, None], img: CryptImage) -> None:
         self.name = name
@@ -20,8 +23,7 @@ class Card:
         return f"<Card name={self.name}, author={self.author}>"
 
     def __str__(self) -> str:
-        strn = (f"card {self.name} by {self.author}\n"
-                f"riddle: {self.riddle}")
+        strn = f"card {self.name} by {self.author}\nriddle: {self.riddle}"
         if self.sol:
             strn += f"\nsol: {self.sol}"
         return strn
@@ -31,7 +33,7 @@ class Card:
         img = CryptImage.create_from_path(path)
         return cls(name, author, riddle, sol, img)
 
-    def serialize(self) -> bytes:
+    """def serialize(self) -> bytes:
         name = self.name.encode()
         len_name = len(name).to_bytes(self.bytesize, "little")
         author = self.author.encode()
@@ -42,11 +44,29 @@ class Card:
         width = self.img.img.size[0].to_bytes(self.bytesize, "little")
         img = self.img.img.tobytes()
         hashed = self.img.hash_key
-        return len_name + name + len_auth + author + height + width + img + hashed + len_ridd + riddle
+        return len_name + name + len_auth + author + height + width + img + hashed + len_ridd + riddle"""
+
+    def serialize(self) -> bytes:
+        name, author, riddle = self.name.encode(), self.author.encode(), self.riddle.encode()
+        width, height = self.img.img.size
+        hashed = self.img.hash_key
+        img = self.img.img.tobytes()
+        packed = struct.pack(self.fmt, len(name), len(author), len(riddle), width, height)
+        packed += name + author + riddle + hashed + img
+        return packed
 
     @classmethod
     def deserialize(cls, data: bytes) -> 'Card':
-        print(len(data))
+        unp = struct.unpack(cls.fmt, data[:cls.leng])
+        lname, lauthor, lriddle = (cls.leng + sum(unp[j] for j in range(i + 1)) for i in range(3))
+        name, author, riddle = data[cls.leng:lname].decode(), data[lname:lauthor].decode(), data[lauthor:lriddle].decode()
+        hashed = data[lriddle:lriddle + cls.hashlen]
+        img = data[lriddle + cls.hashlen:lriddle + cls.hashlen + unp[3] * unp[4] * 3]
+        cimg = CryptImage(Image.frombytes('RGB', (unp[3], unp[4]), img), hashed)
+        return cls(name, author, riddle, None, cimg)
+
+    """@classmethod
+    def deserialize(cls, data: bytes) -> 'Card':
         end = cls.bytesize
         len_name = int.from_bytes(data[:end], "little")
         start, end = end, end + len_name
@@ -60,15 +80,12 @@ class Card:
         start, end = end, end + cls.bytesize
         width = int.from_bytes(data[start:end], "little")
         start, end = end, end + height * width * 3
-        print(height, width)
-        print(len(data[start:end]))
         img = Image.frombytes('RGB', (width, height), data[start:end])
         start, end = end, end + 32
         hash_key = data[start:end]
         start, end = end, end + cls.bytesize
         len_ridd = int.from_bytes(data[start:end], "little")
         start, end = end, end + len_ridd
-        print(data[start:end])
         riddle = data[start:end].decode()
         cimg = CryptImage(img, hash_key)
-        return cls(name, author, riddle, None, cimg)
+        return cls(name, author, riddle, None, cimg)"""
